58998da266edfdbe041530b65c80071000ecd16d
embarcompany <embarcompanymarketing@gmail.com>
2026-07-15 18:59:45 -0300

    Update TrustStrip.tsx

--- src/components/sections/TrustStrip.tsx
+++ src/components/sections/TrustStrip.tsx
@@ -20,7 +20,7 @@ export function TrustStrip() {
     },
     {
       icon: <Award className="w-5 h-5 text-white" />,
-      title: "Especialistas ema",
+      title: "Especialistas em",
       subtitle: "documentação",
     },
   ];
